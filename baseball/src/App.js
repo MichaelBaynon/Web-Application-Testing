c1e8b56bc0f880f2674be1ef57880fcd77805f12
MichaelBaynon <baynonmichael@gmail.com>
2019-08-08 18:06:44 -0400

    wip

---
+++ baseball/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Display from "./components/Display";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Baseball Game</h1>
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ baseball/src/components/Display.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Display() {
+  return (
+    <>
+      <p>Balls: </p>
+      <p>Strikes: </p>
+    </>
+  );
+}
